96c1cebfc91fca9bc6f317518f322d1d040c124d
DonQuixuote <donquixuote@gmail.com>
2026-01-04 21:30:32 -0500

    Update eth_transcan.py
    wallet length input fix

--- eth_transcan.py
+++ eth_transcan.py
@@ -1,8 +1,9 @@
 from web3 import web3 
 #Call for ethereum address for tracking
 wallet_input = input("Enter desired wallet: ")
-#Require 25 characters else no tracking
-if wallet_input != range(0,26):
+#Require 42 characters (eth address length) else no tracking , should start with 0x also
+if len(wallet_input) != 42:
     print (wallet_input)
 else:
+
     print ("Tracking wallet trail")
